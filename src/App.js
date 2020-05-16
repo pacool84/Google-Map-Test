15b4c51ead987e916a54c00dfdba8320b519c615
Francisco López <54652030+pacool84@users.noreply.github.com>
2020-05-15 19:54:05 -0500

    Add map and marker component, also add info for each marker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker,
+  InfoWindow,
+} from "react-google-maps";
+import * as parksData from "./data/location-perks.json";
+import "./App.css";
+
+function Map() {
+  const [selectedPark, setSelectedPark] = useState(null);
+  return (
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 45.421532, lng: -75.697189 }}
+    >
+      {parksData.features.map((park) => (
+        <Marker
+          key={park.properties.PARK_ID}
+          position={{
+            lat: park.geometry.coordinates[1],
+            lng: park.geometry.coordinates[0],
+          }}
+          onClick={() => {
+            setSelectedPark(park);
+          }}
+          /* icon={{
+            url: "/image.png",
+            scaledSize: new window.google.maps.Size(25, 25),  aqui podriamos poner un icono a los marcadores
+          }} */
+        />
+      ))}
+      {selectedPark && (
+        <InfoWindow
+          position={{
+            lat: selectedPark.geometry.coordinates[1],
+            lng: selectedPark.geometry.coordinates[0],
+          }}
+          onCloseClick={() => {
+            setSelectedPark(null);
+          }}
+        >
+          <div>
+            <h1>{selectedPark.properties.NAME}</h1>
+            <p>{selectedPark.properties.DESCRIPTIO}</p>
+            <p>{selectedPark.properties.FACILITY}</p>
+            <p>{selectedPark.properties.FACILITY_F}</p>
+          </div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  );
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <WrappedMap
+        /* googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`} */
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
+        loadingElement={<div style={{ height: "100%" }} />}
+        containerElement={<div style={{ height: "100%" }} />}
+        mapElement={<div style={{ height: "100%" }} />}
+      />
     </div>
   );
 }
